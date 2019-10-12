05f4858eaec6641e59abb138781538be35ce05fe
Vinícius Rodrigues <vinird1993@gmail.com>
2019-10-12 00:11:39 -0300

    Create collection view

--- TheMovie/TheMovie/Features/ Movies/View/Controller/UpcomingMoviesViewController.swift
+++ TheMovie/TheMovie/Features/ Movies/View/Controller/UpcomingMoviesViewController.swift
@@ -10,7 +10,53 @@ import UIKit
 
 class UpcomingMoviesViewController: BaseViewController<UpcomingMovieViewModel> {
 
+    @IBOutlet weak var backGroundView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private let reuseIdentifier = String(describing: MovieCollectionViewCell.self)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib(nibName: reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension UpcomingMoviesViewController : UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension UpcomingMoviesViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MovieCollectionViewCell
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension UpcomingMoviesViewController: UICollectionViewDelegateFlowLayout {
+    ///Set  size of the cell for collection
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let inset:CGFloat = 16
+        return UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 160, height: 250)
     }
 }
